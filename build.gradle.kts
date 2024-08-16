1657d774f066698110d2b4dc71cde9a50dabf25c
dakriy <git@persignum.com>
2024-08-16 14:46:50 -0700

    task: use version catalog and add exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,22 +10,22 @@ repositories {
 }
 
 dependencies {
-    val slf4jVersion = "2.0.16"
-    val konfVersion = "1.1.2"
-
     implementation(kotlin("stdlib-jdk8"))
 
-    implementation("com.uchuhimo", "konf-core", konfVersion)
-    implementation("com.uchuhimo", "konf-yaml", konfVersion)
+    implementation(libs.konfCore)
+    implementation(libs.konfYaml)
 
-    implementation("io.github.oshai", "kotlin-logging-jvm", "7.0.0")
-    implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-core-jvm", "1.8.1")
+    implementation(libs.exposedCore)
+    runtimeOnly(libs.exposedJdbc)
 
-    implementation("org.slf4j", "slf4j-api", slf4jVersion)
-    runtimeOnly("org.slf4j", "slf4j-simple", slf4jVersion)
+    implementation(libs.kotlinLogger)
+    implementation(libs.kotlinCoroutinesCore)
+
+    implementation(libs.slf4j)
+    runtimeOnly(libs.slf4jSimple)
 
     testImplementation(kotlin("test"))
-    testImplementation("io.kotest", "kotest-assertions-core-jvm", "5.9.1")
+    testImplementation(libs.kotestCore)
 }
 
 tasks {
